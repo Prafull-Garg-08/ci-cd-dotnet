2309bea5d4813715deb758b2fc673ba202f8d5e9
Prafull-garg-08 <prafull.garg@sas.dk>
2024-09-09 15:33:13 +0530

    troubleshoot

--- CurrentTimeService/Program.cs
+++ CurrentTimeService/Program.cs
@@ -1,32 +1,17 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-// Handle HTTPS redirection if HTTPS is available
-if (app.Environment.IsProduction())
-{
-    app.UseHttpsRedirection();
-}
+app.UseHttpsRedirection();
 
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapGet("/time/utc", () => Results.Ok(DateTime.UtcNow));
+// GET UTC
+app.MapGet("time/utc", () => Results.Ok(DateTime.UtcNow));
 
 await app.RunAsync();
+

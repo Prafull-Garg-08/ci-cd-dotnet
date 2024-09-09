6d6e95cb26737eb9346e3224fc94934587c79d54
Prafull-garg-08 <prafull.garg@sas.dk>
2024-09-09 15:42:51 +0530

    troubleshoot1

--- CurrentTimeService/Program.cs
+++ CurrentTimeService/Program.cs
@@ -1,32 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-// Handle HTTPS redirection if HTTPS is available
-if (app.Environment.IsProduction())
-{
-    app.UseHttpsRedirection();
-}
+app.UseHttpsRedirection();
 
-app.UseRouting();
-
-app.UseAuthorization();
-
-// Define the GET endpoint for UTC time
-app.MapGet("/time/utc", () => Results.Ok(DateTime.UtcNow));
-
-// Define the GET endpoint for the root URL
-app.MapGet("/", () => Results.Ok("Welcome to the CICD .NET application!"));
+// GET UTC
+app.MapGet("time/utc", () => Results.Ok(DateTime.UtcNow));
 
 await app.RunAsync();

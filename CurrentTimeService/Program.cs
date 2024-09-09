1028940b0e457ee30e257b3420d0547a27372e2f
Prafull-garg-08 <prafull.garg@sas.dk>
2024-09-09 15:47:17 +0530

    root

--- CurrentTimeService/Program.cs
+++ CurrentTimeService/Program.cs
@@ -1,16 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
 
+// Redirect HTTP requests to HTTPS
 app.UseHttpsRedirection();
 
-// GET UTC
-app.MapGet("time/utc", () => Results.Ok(DateTime.UtcNow));
+// Define route for root URL
+app.MapGet("/", () => Results.Ok("Welcome to the CICD .NET application!"));
+
+// Define route for GET UTC time
+app.MapGet("/time/utc", () => Results.Ok(DateTime.UtcNow));
 
 await app.RunAsync();
